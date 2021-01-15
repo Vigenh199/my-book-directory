5699aa47499c777751dd4580ccbc2cb234b3edf8
Vigen <vigenh199@gmail.com>
2021-01-15 11:43:05 +0400

    Fixed mongodb connection string

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Book = require('./models/book.model');
 
 
 MongoClient.connect(
-    'mongodb://Vigenh199:vigenh199@localhost:27017/?authSource=my_apps',
+    'mongodb://Vigenh199:vigenh199@localhost:27017/?authSource=admin',
     { useNewUrlParser: true, useUnifiedTopology: true }
 )
  .then(async (client) => {
